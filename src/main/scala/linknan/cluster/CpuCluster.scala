3aeafe7d4bde8b4ca4e7f375bb68f214d091c727
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2024-11-08 18:04:15 +0800

    fix(cluster): fix icn connection between cluster and noc, when synchronized interface is used.

--- src/main/scala/linknan/cluster/CpuCluster.scala
+++ src/main/scala/linknan/cluster/CpuCluster.scala
@@ -3,6 +3,7 @@ package linknan.cluster
 import chisel3._
 import chisel3.experimental.hierarchy.core.IsLookupable
 import chisel3.experimental.hierarchy.{Definition, Instance, instantiable, public}
+import chisel3.util.ReadyValidIO
 import darecreek.exu.vfu.{VFuParameters, VFuParamsKey}
 import freechips.rocketchip.diplomacy.LazyModule
 import freechips.rocketchip.tilelink.{TLBundle, TLBundleParameters}
@@ -15,6 +16,7 @@ import xiangshan.XSCoreParamsKey
 import xijiang.Node
 import xijiang.router.base.DeviceIcnBundle
 import xs.utils.tl.{TLUserKey, TLUserParams}
+import zhujiang.chi.Flit
 import zhujiang.{ZJParametersKey, ZJRawModule}
 
 class BlockTestIO(val params:BlockTestIOParams)(implicit p:Parameters) extends Bundle {
@@ -53,7 +55,31 @@ class CpuCluster(node:Node)(implicit p:Parameters) extends ZJRawModule {
   @public val icn = IO(new ClusterDeviceBundle(node))
   @public val core = if(removeCore) Some(IO(Vec(node.cpuNum, new BlockTestIO(coreIoParams)))) else None
 
-  icn <> hub.io.icn
+  hub.io.icn.misc <> icn.misc
+  hub.io.icn.osc_clock := icn.osc_clock
+  hub.io.icn.dft := icn.dft
+  hub.io.icn.ccn.reset := icn.ccn.reset
+  hub.io.icn.ccn.async.foreach(_ <> icn.ccn.async.get)
+  if(hub.io.icn.ccn.sync.isDefined) {
+    val hubIcn = hub.io.icn.ccn.sync.get
+    val nocIcn = icn.ccn.sync.get
+    def connChn[T <: Data](sink:Option[ReadyValidIO[T]], src:Option[ReadyValidIO[T]]):Unit = {
+      if(sink.isDefined) {
+        sink.get.valid := src.get.valid
+        src.get.ready := sink.get.ready
+        sink.get.bits := src.get.bits.asTypeOf(sink.get.bits)
+      }
+    }
+    connChn(hubIcn.rx.req, nocIcn.rx.req)
+    connChn(hubIcn.rx.resp, nocIcn.rx.resp)
+    connChn(hubIcn.rx.data, nocIcn.rx.data)
+    connChn(hubIcn.rx.snoop, nocIcn.rx.snoop)
+    connChn(nocIcn.tx.req, hubIcn.tx.req)
+    connChn(nocIcn.tx.resp, hubIcn.tx.resp)
+    connChn(nocIcn.tx.data, hubIcn.tx.data)
+    connChn(nocIcn.tx.snoop, hubIcn.tx.snoop)
+  }
+
   if(removeCsu) {
     hub.io.cluster := DontCare
   } else {
